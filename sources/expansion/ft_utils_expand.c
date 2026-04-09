2b7eec1d5e6f7c7e32b631f6a0baabc17d694d14
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2026-04-09 20:54:26 -0300

    double free no ft_expd_qoutes solve

--- sources/expansion/ft_utils_expand.c
+++ sources/expansion/ft_utils_expand.c
@@ -6,7 +6,7 @@
 /*   By: zimbo <zimbo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 01:13:58 by zimbo             #+#    #+#             */
-/*   Updated: 2026/03/08 03:23:00 by zimbo            ###   ########.fr       */
+/*   Updated: 2026/04/09 20:52:25 by zimbo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,11 +90,10 @@ static char	*ft_append_remaining_text(char *str, int start, int i, char *result)
 	return (result);
 }
 
-char	*ft_expd_quotes(char *str, int *i, t_env *env, int exit)
+char    *ft_expd_quotes(char *str, int *i, t_env *env, int exit)
 {
 	int		start;
 	char	*result;
-	char	*temp;
 
 	(*i)++;
 	start = *i;
@@ -103,11 +102,8 @@ char	*ft_expd_quotes(char *str, int *i, t_env *env, int exit)
 	{
 		if (str[*i] == '$')
 		{
-			temp = result;
 			result = ft_handle_dollar(&(t_dollar_data){str, i,
-					&start, result, env, exit});
-			if (temp != result)
-				free(temp);
+				&start, result, env, exit});
 		}
 		else
 			(*i)++;
