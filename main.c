ed08bb6a376859932e74914e26aa88cad174d0a9
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2026-04-09 20:44:35 -0300

    fazendo os testes

--- main.c
+++ main.c
@@ -6,22 +6,13 @@
 /*   By: zimbo <zimbo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 00:29:30 by zimbo             #+#    #+#             */
-/*   Updated: 2026/03/12 19:57:37 by zimbo            ###   ########.fr       */
+/*   Updated: 2026/04/09 20:35:25 by zimbo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	g_signal = 0;
-
-// static char	**my_completion(const char *text, int start, int end)
-// {
-// 	(void)text;
-// 	(void)start;
-// 	(void)end;
-// 	rl_attempted_completion_over = 1;
-// 	return (NULL);
-// }
+int g_signal = 0;
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -30,10 +21,8 @@ int	main(int argc, char **argv, char **envp)
 	(void)argc;
 	(void)argv;
 	rl_bind_key('\t', rl_complete);
-	// rl_attempted_completion_function = my_completion;
 	env = ft_init_env(envp);
 	ft_setup_interactive_signals();
 	ft_shell_loop(env);
-	ft_free_env(env);
 	return (0);
 }
